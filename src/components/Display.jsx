e8f7511ef42b35ab45f38e4a2be83b5f2db038ed
Aniket Das <aniket.das9062@gmail.com>
2025-03-25 13:02:35 +0530

    Files Patched

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-<<<<<<< HEAD
-import DisplayHome from './DisplayHome'
+import DisplayHome from './Displayhome'
 
 const Display = () => {
   return (
@@ -9,16 +8,6 @@ const Display = () => {
       <Routes>
         <Route path='/' element={<DisplayHome/>} />
         </Routes>
-=======
-import Displayhome from './Displayhome'
-
-const Display = () => {
-  return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0 '>
-      <Routes>
-        <Route path='/' element={<Displayhome />} />
-      </Routes>
->>>>>>> 12b7c70adca6dc55ace970fc159bf65192f1b36a
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-impo<<<<<<< HEAD
 import {BrowserRouter} from 'react-router-dom'
-=======
-import { BrowserRouter } from 'react-router-dom'
->>>>>>> 12b7c70adca6dc55ace970fc159bf65192f1b36a
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
-      <App />
-=======
     <App />
->>>>>>> 12b7c70adca6dc55ace970fc159bf65192f1b36a
-serRouter>
-  </StrictMode>,
+    </BrowserRouter>
+  </StrictMode>
+
 )
